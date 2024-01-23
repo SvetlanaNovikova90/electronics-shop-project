4367847f38a3c620de8fbfb1050e1caed701e736
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-01-23 17:30:17 +0300

    "HW1"

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+class TestItem():
+    pay_rate = 0.5
+    all = []
+    def test_calculate_total_price(self):
+        sc1 = Item('Утюг', 1200, 10)
+        assert sc1.calculate_total_price() == 12000
+        assert sc1.calculate_total_price() != 1200
+    def test_apply_discount(self):
+        sc1 = Item('Утюг', 1200, 10)
+        pay_rate = 0.5
+        assert sc1.apply_discount() == None
