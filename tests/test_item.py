4f9dea1026078c58355f946e558013cb13d3d87e
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-01-26 19:57:17 +0300

    "HW1 2"

--- tests/test_item.py
+++ tests/test_item.py
@@ -11,7 +11,10 @@ class TestItem():
         sc1 = Item('Утюг', 1200, 10)
         assert sc1.calculate_total_price() == 12000
         assert sc1.calculate_total_price() != 1200
-    def test_apply_discount(self):
-        sc1 = Item('Утюг', 1200, 10)
-        pay_rate = 0.5
-        assert sc1.apply_discount() == None
+
+
+def test_apply_discount():
+    sc1 = Item('Утюг', 1200, 10)
+    Item.pay_rate = 0.5
+    sc1.apply_discount()
+    assert sc1.price == 600
